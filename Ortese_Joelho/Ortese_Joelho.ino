61aa6c90b88978f9432f79b221bce0d2df5ecbab
JulioAprígio <julioaprigio8@gmail.com>
2021-03-25 16:42:26 -0300

    adicionando variaveis e funções de envido para a interface Ubidots com MQTT

--- Ortese_Joelho/Ortese_Joelho.ino
+++ Ortese_Joelho/Ortese_Joelho.ino
@@ -6,30 +6,77 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 //=================================================
-
+//============Dados configuração===================
 #define WIFISSID "Paulo Aprigio 2.4GHz"
 #define PASSWORD "Voepaulo08"
-#define TOKEN ""
-#define MQTT_CLIENT_NAME ""
+#define TOKEN "BBFF-Xsta1AUJkVTqMOBkD6xTI8uAQuH1pj"
+#define MQTT_CLIENT_NAME "ortesejoelho"
+//=================================================
+//============Variáveis===========================
+#define VARIABLE_LABEL_POT "potenciometro"
+#define VARIABLE_LABEL_X "eixo-x"
+#define VARIABLE_LABEL_Y "eixo-y"
+#define VARIABLE_LABEL_Z "eixo-z"
+#define DEVICE_LABEL "demo" 
+
+#define pin_pot 13 //GPIO13 para o pino do potenciometro
+
+float eixo_x, eixo_y, eixo_z;
+int potenciometro;
+
+//=================================================
+//=============MQTT Broker=========================
+char mqttBroker[] = "";
+char payload[100];
+char topic[150];
+char topicSubscribe[100];
+char str_sensor[10];
+
+WiFiClient ubidots;
+PubSubClient client(ubidots);
+
+void reconnect(){
+  while(!client.connected()){
+    Serial.println("Tentando conectar MQTT...");
+    if(client.connect(MQTT_CLIENT_NAME,TOKEN,"")){
+      Serial.println("Conectado");
+      client.subscribe(topicSubscribe);
+    }else{
+      Serial.print("Failed,rc=");
+      Serial.print(client.state());
+      Serial.println("Tentando conectar novamente em 2 segundos");
+      delay(2000);
+    }
+  }
+}
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 void checkBNO();
 
-float eixo_x, eixo_y, eixo_z;
-int pot;
+
 void setup() {
+  pinMode(pin_pot,INPUT);
   checkBNO();
   delay(1000);
 }
 
 void loop() {
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  imu::Vector<3> grav = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
-
-  eixo_x = euler.x();
-  eixo_y = euler.y();
-  eixo_z = euler.z();
+  if(!client.connected()){
+    client.subscribe(topicSubscribe);
+    reconnect();
+  }
+  sprintf(topic,"%s%s","/v1.6/devices",DEVICE_LABEL);
+  sprintf(payload,"%s","");
+  sprintf(payload,"{\"%s\":",VARIABLE_LABEL_POT);
+  
+//  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+//  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+//  imu::Vector<3> grav = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+//  potenciometro = analogRead(pin_pot);
+//  
+//  eixo_x = euler.x();
+//  eixo_y = euler.y();
+//  eixo_z = euler.z();
 
 }
 void checkBNO() {
