3187b3a6247e000d13336204bebfbfb46c432ae1
JulioAprígio <julioaprigio8@gmail.com>
2021-03-26 21:07:16 -0300

    adicionando código de teste do potenciometro

--- Teste_Potenciometro/Teste_Potenciometro.ino
+++ Teste_Potenciometro/Teste_Potenciometro.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
-
+const int pin_pot = 13;//pino do ESP32
+int potenciometro = 0;//valor do ADC
+float voltagem = 0;//valor da tensão 
+void setup() 
+{
+Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+potenciometro = analogRead(pin_pot);
+Serial.print("Valor ADC= ");
+Serial.print(potenciometro);
+delay(1000);
+voltagem = (potenciometro * 3.3 ) / (4095);
+Serial.print(" Tensao= ");
+Serial.print(voltagem);
+Serial.println(" V");
+delay(1000);
 }
