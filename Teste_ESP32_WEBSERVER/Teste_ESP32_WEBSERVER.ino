fc4ef70f0e7f1acf18f0aeb1f4c2eccbfe081015
JulioAprígio <julioaprigio8@gmail.com>
2021-04-19 11:36:56 -0300

    Update Teste_ESP32_WEBSERVER.ino, index.html, and 2 more files...

---
+++ Teste_ESP32_WEBSERVER/Teste_ESP32_WEBSERVER.ino
@@ -0,0 +1,12 @@
+//referencia: https://randomnerdtutorials.com/esp32-mpu-6050-web-server/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
